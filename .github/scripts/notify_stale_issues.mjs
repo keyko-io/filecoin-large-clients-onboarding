86af8ae73f23d15782744ca2457bffc35a07e314
Charly López <charly@keyko.io>
2023-08-25 17:40:44 +0200

    Converted to concurrent, added logs

--- .github/scripts/notify_stale_issues.mjs
+++ .github/scripts/notify_stale_issues.mjs
@@ -25,19 +25,30 @@ async function checkAndCommentOnIssues() {
   tenDaysAgo.setDate(tenDaysAgo.getDate() - 10);
   const tenDaysAgoDateString = tenDaysAgo.toISOString().split('T')[0];
 
-  for (const issue of issues) {
-    const updatedAt = new Date(issue.updated_at);
-    const updatedAtDateString = updatedAt.toISOString().split('T')[0];
-
-    if (updatedAtDateString === tenDaysAgoDateString) {
-      await octokit.issues.createComment({
-        owner,
-        repo,
-        issue_number: issue.number,
-        body: "This application has not seen any responses in the last 10 days. This issue will be marked with Stale label and will be closed in 4 days. Comment if you want to keep this application open."
-      });
-    }
-  }
+  Promise.allSettled(
+    issues.map(
+      async (issue) => new Promise(
+        async (resolve, reject) => {
+          const updatedAt = new Date(issue.updated_at);
+          const updatedAtDateString = updatedAt.toISOString().split('T')[0];
+          console.log(updatedAtDateString, tenDaysAgoDateString)
+          if (updatedAtDateString === tenDaysAgoDateString) {
+            await octokit.issues.createComment({
+              owner,
+              repo,
+              issue_number: issue.number,
+              body: "This application has not seen any responses in the last 10 days. This issue will be marked with Stale label and will be closed in 4 days. Comment if you want to keep this application open."
+            });
+            console.log(`Stale advice on issue ${issue.number}`);
+          } else {
+            console.log(`No stale advice on issue ${issue.number}`);
+          }
+          return resolve();
+        }
+      )
+    )
+  );
 }
 
 checkAndCommentOnIssues();
+
