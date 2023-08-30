6ef7da0277bf7ebb7029233f04c850c3da2aa641
Charly López <charly@keyko.io>
2023-08-30 11:54:14 +0200

    Set throttling

--- .github/scripts/close_inactive/close_stale_issues.mjs
+++ .github/scripts/close_inactive/close_stale_issues.mjs
@@ -5,6 +5,8 @@ import fetch from "node-fetch";
 import { getLastComment } from "./utils.mjs";
 
 const DAYS_TO_WAIT = 14;
+const RATE_REMAINING_LIMIT = 100;
+const RATE_SLEEP = 1000;
 const owner = process.env.GITHUB_REPOSITORY.split('/')[0];
 const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
 
@@ -26,55 +28,58 @@ async function closeStaleIssues() {
 
   const dateThreshold = new Date();
   dateThreshold.setDate(dateThreshold.getDate() - DAYS_TO_WAIT);
-  
-  Promise.allSettled(
-    issues.map(
-      async (issue) => new Promise(
-        async (resolve, reject) => {
-          const lastComment = await getLastComment(octokit, owner, repo, issue.number, false);
-          let updatedAt = lastComment 
-            ? new Date(lastComment.created_at) 
-            : new Date(issue.created_at);
 
-          // Let's calculate the difference between the two dates
-          const diffTime = dateThreshold - updatedAt;
-          const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  for (issue of issues) {
+    const rateLimitStatus = await octokit.rateLimit.get();
+    const remaining = rateLimitStatus.data.rate.remaining;
 
-          if (diffDays > 0) {
-            await octokit.issues.createComment({
-              owner,
-              repo,
-              issue_number: issue.number,
-              body: `This application has not seen any responses in the last 14 days, so for now it is being closed. Please feel free to contact the Fil+ Gov team to re-open the application if it is still being processed. Thank you!
+    if (remaining < RATE_REMAINING_LIMIT) {
+      const sleepTime = RATE_SLEEP;
+      console.log(`Issue ${issue.number}: Sleeping for ${sleepTime} ms`);
+      await new Promise(resolve => setTimeout(resolve, sleepTime));
+    }
+    
+    const lastComment = await getLastComment(octokit, owner, repo, issue.number, false);
+    let updatedAt = lastComment 
+      ? new Date(lastComment.created_at) 
+      : new Date(issue.created_at);
+
+    // Let's calculate the difference between the two dates
+    const diffTime = dateThreshold - updatedAt;
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if (diffDays > 0) {
+      await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: issue.number,
+        body: `This application has not seen any responses in the last 14 days, so for now it is being closed. Please feel free to contact the Fil+ Gov team to re-open the application if it is still being processed. Thank you!
 \n\n
 --
 Commented by Stale Bot.`
-            });
-            // Add stale label.
-            await octokit.issues.addLabels({
-              owner,
-              repo,
-              issue_number: issue.number,
-              labels: ['stale']
-            });
+      });
+      // Add stale label.
+      await octokit.issues.addLabels({
+        owner,
+        repo,
+        issue_number: issue.number,
+        labels: ['stale']
+      });
 
-            // Close issue.
-            await octokit.issues.update({
-              owner,
-              repo,
-              issue_number: issue.number,
-              state: 'closed'
-            });
+      // Close issue.
+      await octokit.issues.update({
+        owner,
+        repo,
+        issue_number: issue.number,
+        state: 'closed'
+      });
 
-            console.log(`Let's close issue ${issue.number}. Last commented ${diffDays} days ago`);
-          } else {
-            console.log(`Issue ${issue.number} will remain open. Updated ${diffDays} days ago`);
-          }
-          return resolve();
-        }
-      )
-    )
-  );
+      console.log(`Let's close issue ${issue.number}. Last commented ${diffDays} days ago`);
+    } else {
+      console.log(`Issue ${issue.number} will remain open. Updated ${diffDays} days ago`);
+    }
+  }
 }
+
 closeStaleIssues();
 
